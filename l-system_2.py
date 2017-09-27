826a6ca038001212b1b0c24eb920f0d8160d9d22
Mike <31995833+hanchak@users.noreply.github.com>
2017-09-26 20:28:36 -0400

    Add files via upload
    tweaked some settings

--- l-system_2.py
+++ l-system_2.py
@@ -17,14 +17,16 @@ import matplotlib.pyplot as plt
 
 # dictionary containing the mapping rule
 # this is a tree-like L-System (reference: The Coding Train on YouTube)
+rule = {'F':'FF+[+F-F-F-F]-[-F+F+F+F]'}
 rule = {'F':'FF+[+F-F-F]-[-F+F+F]'}
+#rule = {'F':'F-F++F-F'}
 
 # initial condition
 sentence = 'F'
 new_sentence = ''
 
 # number of recursive iterations
-N = 5
+N = 6
 #################### generate the sentence of moves #####################
 # generate the sentence of moves
 for i in range(N):
@@ -41,7 +43,7 @@ for i in range(N):
 #################### pot the resulting moves as lines ####################
 ######################### using MATPLOTLIB graphics ######################
 l = 20  # length of forward movement
-ang = 20 * np.pi / 180  # turning angle
+ang = 25 * np.pi / 180  # turning angle
 pos = [(0,0)]  # initial position (list of tuples)
 heading = [90]    # initial heading (list of floats)
 
@@ -49,11 +51,13 @@ heading = [90]    # initial heading (list of floats)
 x = 0
 y = 0
 th = 90 * np.pi / 180
-scale = 0.66
+scale = 0.7
 
 colors = plt.get_cmap('viridis')
 
-ax = plt.axes()
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_position([0,0,1,1])
 
 # now create a list of lists to save all of the x,y points for later plotting
 points = [[] for i in range(N+1)]
@@ -61,10 +65,10 @@ level = 0  # level for later coloring
 
 def draw(x,y,th,pos):
     '''get the coordinates of the points that make up a single line'''
-    oldx = x
-    oldy = y
-    x = oldx + l*np.cos(th)
-    y = oldy + l*np.sin(th)
+    #oldx = x
+    #oldy = y
+    x += l*np.cos(th)
+    y += l*np.sin(th)
 
     return x,y
 
@@ -96,23 +100,24 @@ for char in sentence:
     elif char == ']':
         level -= 1
         # go to last saved position
-        x,y = pos[-1]
-        th = heading[-1]
+        #x,y = pos[-1]
+        #th = heading[-1]
         l = l / scale
         
-        # delete last saved position
-        pos.pop()
-        heading.pop()
+        # get then delete last saved position
+        x,y = pos.pop()
+        th = heading.pop()
         
-        points[level].append([np.nan,np.nan])  # break in the plotting
+        #points[level].append([np.nan,np.nan])  # break in the plotting
         points[level].append([x,y])
 
 #### this is the actual plot generation
 for i,point in enumerate(points):
-    
-    pt_array = np.array(point)  # make an array for each level
-
-    plt.plot(pt_array[:,0],pt_array[:,1],linewidth=0.2,color=colors(i / N))
+    if point != []:
+        pt_array = np.array(point)  # make an array for each level
+        
+        plt.plot(pt_array[:,0],pt_array[:,1],linewidth=0.5, \
+                 color=colors(i / N), alpha=0.8)
 
 
 plt.axis('equal')
